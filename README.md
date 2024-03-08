6becd9f94a73133849313dcc520e2db28a6cea50
Darcy <110570447+demccusker@users.noreply.github.com>
2024-03-08 14:02:20 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # disaster_response_nlp
-An NLP model to categorize disaster relief messages 
+### An NLP model to categorize disaster relief messages 
+
+This project uses data provided by [Appen](https://www.appen.com/) about text messages sent during natural disaster. There are two files, messages and categories. The messages file includes the message in the original language as well as a translated version. The categories file contains information about what kind of message was sent; did it have to do with clean water, food, or getting medical help? There are 36 possible categories each message could fall into. 
+
+The purpose of this project is to create a NLP pipeline that can process new messages and predict which category they will fall into, so they can be routed to the correct agency. 
+
+To do so, I first created an ETL pipeline. This pipeline loads the messages and categories files, merges, cleans, and then stores them. This means any new data from future disasters can be loaded into the same pipeline for further analysis. 
+
+Next, I built an ML pipeline to build a model that can make predictions about new messages. This pipeline loads the data, splits it into training and test data, builds a text processing pipeline, then trains and tunes a model. This model can be used to make predictions about what categories new messages will fall into. 
+
+Next steps: build a web app with a simple interface that allows a worker to input a message and receive an answer for what categories it falls into. 
+
+
+### Libraries Used: 
+The libraries used for this project include:
+> pandas, version 2.2.0 <br>
+> scikitlearn, version 1.4 <br>
+> sqlalchemy, version 2.0.28 <br>
+> numpy, version 1.26.4 <br>
+> nltk version 3.6.6 
+
+
---
+++ data/process_data.py
@@ -0,0 +1,71 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+def load_data(messages_filepath, categories_filepath):
+    '''
+    Loads two files of messages and categories into a single concatenated dataframe
+    inputs: messages_filepath, categories_filepath
+    outputs: dataframe
+    '''
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories,how="left",on="id")
+    return df
+
+def clean_data(df):
+    '''
+    Cleans inputed dataframe by seperating out columns for categories into ML readable format and dropping duplicates
+    input: df
+    output: cleaned df
+    '''
+    categories = df['categories'].str.split(";",expand=True)
+    row = categories.iloc[0]
+    category_colnames = row.apply(lambda x: x[:-2])
+    categories.columns = category_colnames
+    for column in categories:
+        categories[column] = categories[column].str.strip().str[-1]
+        categories[column] = categories[column].astype(int)
+    df.drop(labels='categories',axis=1,inplace=True)
+    df = pd.concat([df,categories],axis=1)
+    df.drop_duplicates(inplace=True)
+    return df
+
+def save_data(df, database_filepath):
+    '''
+    Saves data to a SQL database
+    inputs: dataframe and database_filename
+    returns: nothing 
+    '''
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df.to_sql(database_filepath, engine, index=False) 
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
